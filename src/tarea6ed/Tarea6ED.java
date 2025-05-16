2be718158d2a6ff8bfc53227d9f40e69a0921251
pgcar <pgcarla.90@gmail.com>
2025-05-16 20:36:27 +0200

    Añadida nueva clase y modificada Tarea6ed

--- src/tarea6ed/Tarea6ED.java
+++ src/tarea6ed/Tarea6ED.java
@@ -2,13 +2,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
+/**
+ * Clase principal que contiene el método main para ejecutar la aplicación.
+ * 
+ * @author pgcar
+ * @version 1.0
+ */
+
+
+
 package tarea6ed;
 
 /**
  *
  * @author pgcar
  */
+
+
 public class Tarea6ED {
+    
+        /**
+     * Método principal que inicia la aplicación.
+     * 
+     */
 
 public static void main(String[] args) {
 
